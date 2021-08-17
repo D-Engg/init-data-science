66bcf78cafbf6c17f90f872e8a1be325b4dfe5f5
D-Engg <85694604+D-Engg@users.noreply.github.com>
2021-08-17 09:38:17 +0530

    Delete HackerRank Solutions/Solutions directory

--- HackerRank Solutions/Solutions/Doormat.py
+++
@@ -1,3 +0,0 @@
-ln, wd = map(int, input().split())
-pattern = [('.|.'* (2*i + 1)).center(wd, '-') for i in range(ln//2)]
-print('\n'.join(pattern + ['WELCOME'.center(wd, '-')] + pattern[: : -1]))
--- HackerRank Solutions/Solutions/Itertools_Combinations.py
+++
@@ -1,7 +0,0 @@
-from itertools import combinations
-
-A, n = input().split()
-
-for i in range(1, int(n) + 1):
-    for j in combinations(sorted(A), i):
-        print(''.join(j))
